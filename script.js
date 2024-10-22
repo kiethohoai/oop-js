aa56ffb0e567cb55e5ee774812a8c1c15fc57dbd
kiethohoai <kietho.060193@gmail.com>
2024-10-22 10:32:03 +0700

    008 Prototypal Inheritance on Built-In Objects

--- script.js
+++ script.js
@@ -20,30 +20,51 @@ Person.prototype.calcAge = function () {
 const john = new Person('John', 1994);
 const mini = new Person('Mini', 1997);
 
-console.log(`🚀  john =>`, john);
-console.log(`🚀  mini =>`, mini);
+// console.log(`🚀  john =>`, john);
+// console.log(`🚀  mini =>`, mini);
 
 const jay = {
   fName: 'Jay',
   bYear: 2000,
 };
-console.log(jay instanceof Person); //False
-console.log(john instanceof Person); //True
+// console.log(jay instanceof Person); //False
+// console.log(john instanceof Person); //True
 
-console.log(Person.prototype);
-console.log(john.__proto__);
-console.log(Person.prototype === john.__proto__); //True
-console.log(Person.prototype.isPrototypeOf(john)); //True
-console.log(Person.prototype.isPrototypeOf(mini)); //True
-console.log(Person.prototype.isPrototypeOf(Person)); //False
-john.calcAge();
+// console.log(Person.prototype);
+// console.log(john.__proto__);
+// console.log(Person.prototype === john.__proto__); //True
+// console.log(Person.prototype.isPrototypeOf(john)); //True
+// console.log(Person.prototype.isPrototypeOf(mini)); //True
+// console.log(Person.prototype.isPrototypeOf(Person)); //False
+// john.calcAge();
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(john, mini);
-console.log(john.species, mini.species);
+// console.log(john, mini);
+// console.log(john.species, mini.species);
 
-console.log(john.hasOwnProperty('fName')); //true
-console.log(john.hasOwnProperty('bYear')); //true
-console.log(john.hasOwnProperty('species')); //false
+// console.log(john.hasOwnProperty('fName')); //true
+// console.log(john.hasOwnProperty('bYear')); //true
+// console.log(john.hasOwnProperty('species')); //false
+// ===================================================
+// ===================================================
+// Object.prototype (top of prototype chain)
+console.log(john.__proto__);
+console.log(john.__proto__.__proto__);
+console.log(john.__proto__.__proto__.__proto__);
 
-console.log(`===================================================`);
+console.dir(Person.prototype.constructor);
+
+const arr = [2, 3, 4, 5, 6, 7];
+console.log(arr.__proto__);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__ === Array.prototype); //True
+
+// Don't do this!
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
