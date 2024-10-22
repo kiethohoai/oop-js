ed68c3c87d3da0b66d596e7052e41020ff5417cc
kiethohoai <kietho.060193@gmail.com>
2024-10-22 09:34:26 +0700

    005 Constructor Functions and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+//todo 005 Constructor Functions and the new Operator
+const Person = function (fName, bYear) {
+  // Instance Properties
+  this.fName = fName;
+  this.bYear = bYear;
+
+  // Instance Methods (BAD PRACTICE)
+  // this.calcAge = function () {
+  //   console.log(2024 - this.bYear);
+  // };
+};
+
+// Instance Methods
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.bYear);
+};
+
+const john = new Person('John', 1994);
+const mini = new Person('Mini', 1997);
+
+console.log(`🚀  john =>`, john);
+console.log(`🚀  mini =>`, mini);
+
+const jay = {
+  fName: 'Jay',
+  bYear: 2000,
+};
+console.log(jay instanceof Person); //False
+console.log(john instanceof Person); //True
+
+john.calcAge();
